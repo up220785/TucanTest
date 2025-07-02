3f1e7ca4de0306dc7ed579babaea78e81a61df69
Izel Gallegos <up220785@alumnos.upa.edu.mx>
2025-07-02 17:54:14 -0600

    feat(login): initial login

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "your_secret_key"  
 
     from . import routes
     app.register_blueprint(routes.bp)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,49 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for, render_template_string, session
 
 bp = Blueprint('main', __name__)
+users = {}  
 
 @bp.route('/')
 def home():
-    return "Hello, TucanTest!"
+    if 'user' in session:
+        return f"Hello, {session['user']}! <a href='/logout'>Logout</a>"
+    return "Hello, TucanTest! <a href='/login'>Login</a> <a href='/register'>Register</a>"
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            return "User already exists!"
+        users[username] = password
+        return redirect(url_for('main.login'))
+    return render_template_string('''
+        <form method="post">
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" value="Register">
+        </form>
+    ''')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if users.get(username) == password:
+            session['user'] = username
+            return redirect(url_for('main.home'))
+        return "Invalid credentials!"
+    return render_template_string('''
+        <form method="post">
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    ''')
+
+@bp.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('main.home'))
