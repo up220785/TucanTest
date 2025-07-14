615285f2ef1e07eb26edf56cb4290e5bbe26eb11
Izel Gallegos <up220785@alumnos.upa.edu.mx>
2025-07-14 16:30:27 -0600

    feat(backend): connection to database and basic users route

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, g
 from flask_cors import CORS
+import sqlite3
+
+DATABASE = "tucantestschema.db"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.execute("PRAGMA foreign_keys = ON")  
+    return db
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/members")
-def members():
-    return {"members": ["member1", "member2", "member3"]}
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
