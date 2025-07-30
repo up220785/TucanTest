0dbd7cdb2c2c14e211c78b214b4b7ce87fd7e76d
EdgarMMontes <up220275@alumnos.upa.edu.mx>
2025-07-29 18:01:52 -0600

    mejoras

--- tucan-front/src/App.tsx
+++ tucan-front/src/App.tsx
@@ -1,12 +1,14 @@
 // src/App.tsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Homepage from "./pages/Homepage";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Homepage from "./pages/homepage";
+import Index from "./pages/index";
 
 const App = () => (
   <Routes>
+    <Route path="/" element={<Index />} />
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<Register />} />
     <Route path="/homepage" element={<Homepage />} />
